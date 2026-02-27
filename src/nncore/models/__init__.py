8c383c979e898d821daa03d273d52dadc644fe8b
zakklotz <zakklotz@gmail.com>
2026-02-27 04:08:54 -0500

    Add phase 2 config scaffolding with safe defaults

--- src/nncore/models/__init__.py
+++ src/nncore/models/__init__.py
@@ -1,9 +1,19 @@
-from .config import AttentionConfig, BlockConfig, TransformerConfig
+from .config import (
+    AttentionConfig,
+    BlockConfig,
+    ConstraintConfig,
+    MoEConfig,
+    TajalliConfig,
+    TransformerConfig,
+)
 from .transformer import Transformer, TransformerDecoderBlock
 
 __all__ = [
     "AttentionConfig",
     "BlockConfig",
+    "ConstraintConfig",
+    "MoEConfig",
+    "TajalliConfig",
     "TransformerConfig",
     "Transformer",
     "TransformerDecoderBlock",
